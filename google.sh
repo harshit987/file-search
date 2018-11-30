b980296036ddf068fc8fcc46359acc09d318b22a
Harshit kumar <hrk27071999@gmail.com>
2018-11-30 10:04:46 +0530

    third commit

--- google.sh
+++ google.sh
@@ -3,12 +3,20 @@
 echo "Hello World!"
 File1=$1
 File1="$File1/"
-
+File2=$2
+File2="$File2/"
 for d in */; do
 
 	if [[ $d = $File1 ]]
     then
       cd ./$d
+      for e in */; do
+          if [[ $e = $File2 ]]
+          then
+             
+             cd ./$e
+          fi
+      done
        
     fi
 
